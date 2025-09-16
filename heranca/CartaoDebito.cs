4b235b8f4438c6976714fccd8d0b21c0c1f6f4b9
isabellasena13 <isabellasena05@gmail.com>
2025-09-16 00:32:59 +0000

    feat: classe cartao

--- heranca/CartaoDebito.cs
+++ heranca/CartaoDebito.cs
@@ -10,6 +10,21 @@ public class CartaoDebito
     public DateTime Vencimento { get; set; }
     public string Portador { get; set; }
     public string Cvv { get; set; }
+
+    public class bool Debitar(decimal valor)
+    {
+        if (valor > saldo)
+            return false;
+    }
+
+    public bool Debitar(decimal valor, bool usarLimite)
+{
+        if (usarLimite)
+    {
+         if (Valor <= (Saldo + limite))
+        
+    }    
+}
 }
 
 
